941d04bb4ee41e46805d8c5a146155b4497c4697
fshangala <fshangala@gmail.com>
2021-07-21 17:06:43 +0200

    setup complete

--- sis/schools/admin.py
+++ sis/schools/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.School)
+admin.site.register(models.Programme)
+admin.site.register(models.Course)
--- sis/schools/models.py
+++ sis/schools/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=200)
+
+class Programme(models.Model):
+    name = models.CharField(max_length=200)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+    programme = models.ForeignKey(Programme, on_delete=models.CASCADE)
