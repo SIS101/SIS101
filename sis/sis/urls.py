1665a6a912287fceba3650b4400c7a0398fc1853
fshangala <fshangala@gmail.com>
2021-07-22 12:11:40 +0200

    template changed and website added to the system

--- sis/sis/urls.py
+++ sis/sis/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admissions/', include("admissions.urls"))
+    path('admissions/', include("admissions.urls")),
+    path('', include('website.urls'))
 ]
--- sis/website/views.py
+++ sis/website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    template = "website/home.html"
+    context = {}
+
+    return render(request, template, context)
