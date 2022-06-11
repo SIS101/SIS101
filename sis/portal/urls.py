b7f28714721cebb521611a366c9085e016aeb4a9
fshangala <fshangala@gmail.com>
2022-06-11 11:47:57 +0200

    permissions configured

--- sis/portal/urls.py
+++ sis/portal/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('coming-soon/', views.coming_soon, name="coming-soon"),
     path('login/', views.login_page, name="login"),
     path('logout/', views.logout_user, name="logout"),
+    path('permission-denied/', views.permission_denied, name="permission-denied"),
     path('', views.dashboard, name="dashboard")
 ]
